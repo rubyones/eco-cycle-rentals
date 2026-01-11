cb75406603d977d00b63412e73293e7674657d23
Ruby Grace Oñes <rubygrace.ones@jmc.edu.ph>
2026-01-11 04:57:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8627791437-953da",
+  "appId": "1:69754803578:web:85b54ba36d1a48736b72c3",
+  "apiKey": "AIzaSyBwU9sAIWqyaWi-KfM5OW_lUIEkQF3RuxE",
+  "authDomain": "studio-8627791437-953da.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "69754803578"
+};
